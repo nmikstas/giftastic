c0ce668261b665e58f1476a6451879d7a5697e5a
NickM <nmikstas@alumni.scu.edu>
2019-09-19 00:05:58 -0600

    Finished the favorites part of the webpage

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -187,9 +187,6 @@ class ShowGifs
                     var urlDiv = $("<div>");
                     copyDiv.append(urlDiv);
 
-                    //Need this to copy links.
-                    var copySelf = urlDiv;
-
                     //Add event listener to the copy button.
                     copyButton.on("click", function()
                     {
@@ -205,10 +202,10 @@ class ShowGifs
                         if(self.debug)console.log("data URL: " + dataURL);
 
                         //Perform the copy and then remove the temporary text field.
-                        copySelf.append(tempText);
+                        urlDiv.append(tempText);
                         tempText.select();
                         document.execCommand("copy");
-                        copySelf.empty();
+                        urlDiv.empty();
                     });
 
                     /********************* Favorite *********************/
@@ -288,10 +285,148 @@ class Favorites
                 console.log("Favorites title: " + title);
             }
 
+            //Create the gif div.
+            var gifDiv = $("<div>");
+            gifDiv.addClass("gif-div");
 
+            //Get the gif image add it to the gif div.
+            var gifImage = $("<img>");
+            gifImage.attr("src", stillURL);
+            gifImage.attr("alt", title);
+            gifImage.attr("data-still", stillURL);
+            gifImage.attr("data-animate", animatedURL);
+            gifImage.attr("data-state", "still");
 
+            //Add the gif click event handler for animating the gif.
+            gifImage.on("click", function()
+            {
+                var animURL = $(this).attr("data-animate");
+                var stillURL = $(this).attr("data-still");
+                if($(this).attr("data-state") === "still")
+                {
+                    $(this).attr("data-state", "animate");
+                    $(this).attr("src", animURL);
+                }
+                else
+                {
+                    $(this).attr("data-state", "still");
+                    $(this).attr("src", stillURL);
+                }
+            });
 
+            /********************* Overlay **********************/
+            //Create an overlay div.
+            var overlay = $("<div>");
+            overlay.addClass("overlay");
 
+            /********************** Rating **********************/
+            //Create rating div.
+            var ratingDiv = $("<div>");
+            ratingDiv.addClass("gif-overlay-rating");
+
+            //Create rating text and append to the rating div.
+            var ratingText = $("<h4>");
+            ratingText.text("Rating: " + rating);
+            ratingDiv.append(ratingText);
+
+            //Add rating text to the overlay.
+            overlay.append(ratingDiv);
+
+            //Create rating tooltip.
+            var ratingTooltip = $("<div>");
+            ratingTooltip.addClass("tooltiptext");
+            ratingTooltip.text("Image rating");
+
+            //Add rating tooltip to the rating div.
+            ratingDiv.append(ratingTooltip);
+
+            /*********************** Copy ***********************/
+            //Create copy div.
+            var copyDiv = $("<div>");
+            copyDiv.addClass("gif-overlay-copy");
+
+            //Create the copy button and append to the copy div.
+            var copyButton = $("<input>");
+            copyButton.addClass("btn btn-light");
+            copyButton.attr("value", "copy");
+            copyButton.attr("type", "button");
+            copyButton.attr("data-url", gifImage.attr("data-animate"));
+            copyDiv.append(copyButton);
+
+            //Add copy div to the overlay.
+            overlay.append(copyDiv);
+
+            //Create the copy button tooltip.
+            var copyTooltip = $("<div>");
+            copyTooltip.addClass("tooltiptext");
+            copyTooltip.text("Copy URL to Clipboard");
+
+            //Add copy tooltip to the copy div.
+            copyDiv.append(copyTooltip);
+
+            //Create a copy URL div for copy purposes.
+            var urlDiv = $("<div>");
+            copyDiv.append(urlDiv);
+
+            //Add event listener to the copy button.
+            copyButton.on("click", function()
+            {
+                //Create variable to hold URL to copy.
+                var dataURL = $(this).attr("data-url");
+                      
+                //Create text field to copy the value to.
+                var tempText = $("<input>");
+                tempText.attr("type", "text");
+                tempText.attr("id", "temp-text");
+                tempText.attr("value", dataURL);
+
+                if(self.debug)console.log("data URL: " + dataURL);
+
+                //Perform the copy and then remove the temporary text field.
+                urlDiv.append(tempText);
+                tempText.select();
+                document.execCommand("copy");
+                urlDiv.empty();
+            });
+
+            /********************** Remove **********************/
+            //Create remove div.
+            var remDiv = $("<div>");
+            remDiv.addClass("gif-overlay-add");
+
+            //Create the remove button and append to the remove div.
+            var remButton = $("<input>");
+            remButton.addClass("btn btn-light");
+            remButton.attr("value", "X");
+            remButton.attr("type", "button");
+            remDiv.append(remButton);
+
+            //Add remove div to the overlay.
+            overlay.append(remDiv);
+
+            //Create the remove button tooltip.
+            var remTooltip = $("<div>");
+            remTooltip.addClass("tooltiptext");
+            remTooltip.text("Remove From Favorites");
+
+            //Add remove tooltip to the remove div.
+            remDiv.append(remTooltip);
+ 
+            //Add event listener to the remove button.
+            remButton.on("click", function()
+            {
+                //Clear the gifDiv.
+                gifDiv.empty();
+            });
+
+            //Add the gif image to the gif div.
+            gifDiv.append(gifImage);
+
+            //Add the overlay to the gif div.
+            gifDiv.append(overlay);
+
+            //Add the gif div to the webpage.
+            self.div.prepend(gifDiv);
         }
     }
 }
